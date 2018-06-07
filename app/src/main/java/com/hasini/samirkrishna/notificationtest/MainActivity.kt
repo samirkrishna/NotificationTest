fcf811fd0087a39c22d55ffc84b395b6d2278f91
samirkrishna <38221677+samirkrishna@users.noreply.github.com>
2018-06-07 11:50:08 +0530

    Initial commit

---
+++ app/src/main/java/com/hasini/samirkrishna/notificationtest/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.hasini.samirkrishna.notificationtest
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun notify(v:View)
+    {
+        var nManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        var nBuilder=NotificationCompat.Builder(this,"")
+        nBuilder.setTicker("Welcome To JNTUHCEJ")
+        nBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        nBuilder.setContentTitle("Emergency Meeting")
+        nBuilder.setSubText("Administrative Block")
+        nBuilder.setContentText("Emergency Meeting")
+        nBuilder.setAutoCancel(true)
+
+        var i =Intent(this,MainActivity::class.java)
+        var pIntent=PendingIntent.getActivity(this,0,i,0)
+        nBuilder.setContentIntent(pIntent)
+
+        nManager.notify(123,nBuilder.build())//to set notification to status bar
+    }
+}
